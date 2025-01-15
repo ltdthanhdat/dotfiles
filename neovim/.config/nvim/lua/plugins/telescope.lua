fb2a7db3813845980c79ab5da81227012e012d40
lethanhdat <lethanhdat.me@gmail.com>
2025-01-15 22:47:10 +0700

    show hidden file telescope

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -4,8 +4,15 @@ return {
 		'nvim-lua/plenary.nvim'
 	},
 	config = function()
+		require('telescope').setup({
+			defaults = {
+				file_ignore_patterns = { ".git/" }
+			}
+		})
 		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+		vim.keymap.set('n', '<leader>ff', function()
+			vim.cmd('Telescope find_files hidden=true')
+		end, {})
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
